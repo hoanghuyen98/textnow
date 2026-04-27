57859722cae5d95935c8143fca998e10060cea7a
Huyen Hoang <42302034+hoanghuyen98@users.noreply.github.com>
2026-04-27 19:58:39 +0700

    config 6 worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 3 sideline.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 6 sideline.wsgi:application
